69ad5eb6905800fa43299bdc89d3220923980939
Addala Raed <addala.raed@gmail.com>
2025-02-07 11:02:33 +0100

    SRP

---
+++ SRP/src/com/directi/training/srp/exercise_refactored/CarManager.java
@@ -0,0 +1,30 @@
+package com.directi.training.srp.exercise_refactored;
+
+public class CarManager {
+    private final CarRepository _carRepository = new CarRepository();
+
+    public Car getCar(final String carId) {
+        return _carRepository.getFromDb(carId);
+    }
+
+    public String getCarsNames() {
+        StringBuilder sb = new StringBuilder();
+        for (Car car : _carRepository.getAllFromDb()) {
+            sb.append(car.getBrand());
+            sb.append(" ");
+            sb.append(car.getModel());
+            sb.append(", ");
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    public Car getBestCar() {
+        Car bestCar = null;
+        for (Car car : _carRepository.getAllFromDb()) {
+            if (bestCar == null || car.getModel().compareTo(bestCar.getModel()) > 0) {
+                bestCar = car;
+            }
+        }
+        return bestCar;
+    }
+}
